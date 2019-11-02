de3faf118a84d10bf4842c0d8ffbc0aa00f58c46
sethukumar0001 <boylooksrock@gmail.com>
2019-11-02 09:47:09 +0530

    flex box basics

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="flex">
+        <div id="item1">1</div>
+        <div id="item2">2</div>
+        <div id="item3">3</div>
+        <div class="item4">
+        <div>4</div>
+        <div>5</div>
+        </div>
+        <div id="item6">6</div>
+        <div id="item7">7</div>
+        <div id="item8">8</div>
+    </div>
+  );
+}
+
+export default App;
